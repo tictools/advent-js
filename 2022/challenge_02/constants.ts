c6c42269e22b49ba8cd0122c99bfc8e047beb8d9
tictools <jaumevillarreal@gmail.com>
2022-12-03 18:44:38 +0100

    Extract types and constants

---
+++ 2022/challenge_02/constants.ts
@@ -0,0 +1,9 @@
+export const DAYS_OF_THE_WEEK_INDEX = {
+  Sunday: 0,
+  Monday: 1,
+  Tuesday: 2,
+  Wednesay: 3,
+  Thursday: 4,
+  Friday: 5,
+  Saturday: 6,
+};
---
+++ 2022/challenge_02/types.d.ts
@@ -0,0 +1,4 @@
+export type countHoursType = {
+  year: number;
+  holidays: string[];
+};
