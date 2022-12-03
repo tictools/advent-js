1a901d73a388f203cd01667f1c9188c7fb651189
Jaume Villarreal <jaumevillarreal@gmail.com>
2022-12-03 20:15:43 +0100

    Update isWeekendDay.ts

--- 2022/challenge_02/src/helpers/isWeekendDay.ts
+++ 2022/challenge_02/src/helpers/isWeekendDay.ts
@@ -3,6 +3,7 @@ import { DAYS_OF_THE_WEEK_INDEX } from "../constants";
 type dayIndex = {
   dayIndex: number;
 };
+
 const isWeekendDay = ({ dayIndex }: dayIndex): boolean =>
   dayIndex === DAYS_OF_THE_WEEK_INDEX.Saturday ||
   dayIndex === DAYS_OF_THE_WEEK_INDEX.Sunday;
