46bb6855a5604d5f165eea11c52d4b9f1c969dab
tictools <jaumevillarreal@gmail.com>
2022-12-03 19:07:42 +0100

    Define types for getDayFromDate

--- 2022/challenge_02/helpers/getMMDDYYDate.ts
+++ 2022/challenge_02/helpers/getMMDDYYDate.ts
@@ -1,3 +1,8 @@
-const getMMDDYYDate = ({ monthDayString, year }) => `${monthDayString}/${year}`;
+type MMDDYYDate = {
+  monthDayString: string;
+  year: number;
+};
+const getMMDDYYDate = ({ monthDayString, year }: MMDDYYDate): string =>
+  `${monthDayString}/${year}`;
 
 export default getMMDDYYDate;
